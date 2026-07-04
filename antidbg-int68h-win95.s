2a33b82adcb94dd1694bd653c76c07fd9c6f07c8
jrf-g <jrfaragalla@gmail.com>
2026-07-03 17:09:09 -0700

    Improve debugger check and output handling
    Refactor debugger detection logic and message output.

---
+++ antidbg-int68h-win95.s
@@ -0,0 +1,24 @@
+.data
+msga db "debugger detected", 0x0A, 0
+msgb db "no debugger", 0x0A, 0
+.text
+global
+_start:
+  mov ah, 43h
+  int 68h
+  cmp ax, 0f386h
+  jz debugger
+  jnz nodebugger
+
+debugger:
+  mov ah, 09h
+  mov dx, offset msga
+  int 21h
+  mov ax, 4C00h
+  int 21h
+nodebugger:
+  mov ah, 09h
+  mov dx, offset msgb
+  int 21h
+  mov ax, 4C00h
+  int 21h
